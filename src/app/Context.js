30d599cb46d4df4ddd118a8ea621d0467e343163
fares mohamed <fm883254@gmail.com>
2025-03-28 08:25:48 +0200

    fixing

--- src/app/Context.js
+++ src/app/Context.js
@@ -8,14 +8,9 @@ export const AppProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [isLoged, setisLoged] = useState(false);
   const [isLogedDash, setisLogedDash] = useState(false);
-<<<<<<< HEAD
   const [menuAccView, setMenuAccView] = useState(false);
   return (
-    <AppContext.Provider value={{ user, setUser,isLoged, setisLoged,isLogedDash, setisLogedDash,menuAccView, setMenuAccView}}>
-=======
-  return (
     <AppContext.Provider value={{ user, setUser,isLoged, setisLoged,isLogedDash, setisLogedDash}}>
->>>>>>> 75a44478f9bb18d179930d8d031cbb51a63c9256
       {children}
     </AppContext.Provider>
   );
